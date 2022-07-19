dfae05e01e80a8b1ab41fcbf950f6afa7cdb9a4a
Isaac Lupin <95302944+GatorBash@users.noreply.github.com>
2022-07-19 12:18:29 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget -O- https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
+wait
+echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
+wait
+sudo apt update && sudo apt install signal-desktop
+wait
+echo "done"
