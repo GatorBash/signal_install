37c21c8cbfe3b20bf568723d7c85061b15b4a64b
Isaac Lupin <95302944+GatorBash@users.noreply.github.com>
2022-07-19 12:29:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "this will just take a moment"
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | sudo apt-key add -
 wait
 echo "deb [arch=amd64] https://updates.signal.org/desktop/apt xenial main" | sudo tee -a /etc/apt/sources.list.d/signal-xenial.list
